75dab5271679f5a906c9ae8072bd27db4c4c7d12
hamam massarweh <hamam.massarweh@hotmail.com>
2017-06-18 14:08:06 +0300

    Tests Passed! Notes Deleted!

--- src/OOP/Solution/Multiple/OOPMultipleControl.java
+++ src/OOP/Solution/Multiple/OOPMultipleControl.java
@@ -137,7 +137,6 @@ public class OOPMultipleControl
                             minimumCastMethod = method;
                             minCast = curCastInt;
                             candidates.clear();
-//                            inCur = true;
                         }
                     }
                 }
@@ -171,82 +170,6 @@ public class OOPMultipleControl
 
         return null;
     }
-
-
-    //TODO: delete func when done!
-//    public static HashSet<MethodWrapper> func(Class<?> cl, Object[] args) throws OOPMultipleException
-//    {
-//        Class<?>[] supers = cl.getInterfaces();
-//        HashSet<MethodWrapper> currMethods = new HashSet<>();
-//        if (supers.length == 0)
-//        {
-//            for (Method method : cl.getMethods())
-//            {
-//                currMethods.add(new MethodWrapper(method));
-//            }
-//            return currMethods;
-//        }
-//
-//        HashSet<Pair<Class<?>, Method>> candidates = new HashSet<Pair<Class<?>, Method>>();
-//        int expDiffer = Integer.MAX_VALUE;
-//
-//        for (Class<?> parent : supers)
-//        {
-//            HashSet<MethodWrapper> parentMethods = new HashSet<>();
-//            parentMethods = func(parent, args);
-//
-//            for (MethodWrapper methodWrapper : parentMethods)
-//            {
-//                for (MethodWrapper curMethodWrapper : currMethods)
-//                {
-//                    int firstDiffir1 = GetArgsDiffirence(args, methodWrapper.getMethod().getParameterTypes());
-//                    int firstDiffir2 = GetArgsDiffirence(args, curMethodWrapper.getMethod().getParameterTypes());
-//
-//                    if (curMethodWrapper.equals(methodWrapper) || curMethodWrapper.equalsWithDiffer(methodWrapper, firstDiffir1, firstDiffir2))
-//                    {
-//                        candidates.add(new Pair<>(methodWrapper.getMethod().getDeclaringClass(), methodWrapper.getMethod()));
-//                        candidates.add(new Pair<>(curMethodWrapper.getMethod().getDeclaringClass(), curMethodWrapper.getMethod()));
-//                        if (curMethodWrapper.equalsWithDiffer(methodWrapper, firstDiffir1, firstDiffir2))
-//                        {
-//                            expDiffer = firstDiffir1;
-//                        }
-////                        for (Object arg:args)
-////                        {
-////                            System.out.println(arg.toString());
-////                        }
-////                        System.out.println(methodWrapper.getMethod().getDeclaringClass().toString() + methodWrapper.getMethod().toString());
-////                        System.out.println(curMethodWrapper.getMethod().getDeclaringClass().toString() + curMethodWrapper.getMethod().toString());
-//                    }
-//                }
-//            }
-//            currMethods.addAll(parentMethods);
-//        }
-//
-//        if (candidates.size() != 0)
-//        {
-//            throw new OOPCoincidentalAmbiguity(candidates);
-//        }
-//
-//        HashSet<MethodWrapper> toReturnMethods = new HashSet<>();
-//        for (MethodWrapper methodWrapper : currMethods)
-//        {
-//            toReturnMethods.add(methodWrapper);
-//        }
-//
-//        for (Method method : cl.getMethods())
-//        {
-//            if (method.getDeclaringClass() != cl)
-//            {
-//                continue;
-//            }
-//            for (MethodWrapper methodWrapper : toReturnMethods)
-//            {
-//                if (methodWrapper.equals(new MethodWrapper(method))) currMethods.remove(methodWrapper);
-//            }
-//            currMethods.add(new MethodWrapper(method));
-//        }
-//        return currMethods;
-//    }
 
     private static int GetArgsDiffirence(Object[] args1, Class<?>[] args2)
     {
@@ -305,54 +228,6 @@ public class OOPMultipleControl
         return counter;
     }
 
-//    public Object invoke(String methodName, Object[] args) throws OOPMultipleException
-//    {
-//
-//        HashSet<MethodWrapper> funcsSet = func(interfaceClass, args);
-//        MethodWrapper minimumCastMethod = null;
-//        int minCast = Integer.MAX_VALUE;
-//
-//        for (MethodWrapper methodWrapper : funcsSet)
-//        {
-//            if (methodWrapper.getMethod().getName() == methodName)
-//            {
-//                int curCastInt = GetArgsDiffirence(args, methodWrapper.getMethod().getParameterTypes());
-//                if (curCastInt != -1 && curCastInt <= minCast)
-//                {
-//                    if (curCastInt == minCast)
-//                    {
-//                        if (minimumCastMethod.getMethod().getDeclaringClass().isAssignableFrom(methodWrapper.getMethod().getDeclaringClass()))
-//                        {
-//                            minimumCastMethod = methodWrapper;
-//                            minCast = curCastInt;
-//                        }
-//                    }
-//                    else
-//                    {
-//                        minimumCastMethod = methodWrapper;
-//                        minCast = curCastInt;
-//                    }
-//                }
-//            }
-//        }
-//
-//        if (minimumCastMethod != null)
-//        {
-//            try
-//            {
-//                Method classMethod = getClassMethod(minimumCastMethod.getMethod());
-//                Class<?> methodClass = getClassMethod(minimumCastMethod.getMethod()).getDeclaringClass();
-//                Object toReturn = classMethod.invoke(methodClass.newInstance(), args);
-//                return toReturn;
-//            } catch (Exception e)
-//            {
-//                System.out.println(e.getMessage());
-//            }
-//        }
-//
-//        return null;
-//    }
-
     public static String getClassName(String interfaceName)
     {
         int indexOfLastPoint = interfaceName.lastIndexOf(".");
@@ -380,7 +255,6 @@ public class OOPMultipleControl
             MethodWrapper currentMethodWrapper = new MethodWrapper(currentMethod);
             if (currentMethodWrapper.equals(paramMethodWrapper))
             {
-                // System.out.println(currentMethod);
                 return currentMethod;
             }
         }
@@ -443,35 +317,6 @@ public class OOPMultipleControl
         queue.add(cl);
         types.add(cl);
 
-//        Class<?>[] supers = cl.getInterfaces();
-//        if (supers != null)
-//        {
-//            for (Class<?> next : supers)
-//            {
-//                HasInherentAmbiguity(next);
-//            }
-//        }
-//        else
-//        {
-//            return;
-//        }
-
-//        As Written in FAQ
-//        if (!cl.isAnnotationPresent(OOPMultipleInterface.class))
-//        {
-//            throw new OOPBadClass(cl); // change c1 to interfaceClass
-//        }
-
-//        Method[] clMethods = cl.getDeclaredMethods();
-//        for (Method method : clMethods)
-//        {
-//            if (!method.isAnnotationPresent(OOPMultipleMethod.class))
-//            {
-//                throw new OOPBadClass(method); // change c1 to interfaceClass
-//            }
-//        }
-
-        //BFS:
         while (!queue.isEmpty())
         {
             Class<?> curr = queue.poll();
@@ -499,17 +344,6 @@ public class OOPMultipleControl
                     }
                 }
 
-//                if (types.contains(next))
-//                {
-//                    for (Method method : nextMethods)
-//                    {
-//                        if (!Modifier.isPrivate(method.getModifiers()))
-//                        {
-//                            throw new OOPInherentAmbiguity(cl, next, method); // change c1 to interfaceClass
-//                        }
-//                    }
-//                }
-
                 if (!types.contains(next))
                 {
                     types.add(next);
@@ -529,133 +363,3 @@ public class OOPMultipleControl
         }
     }
 }
-//
-//    @OOPMultipleInterface
-//    interface C
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//
-//    @OOPMultipleInterface
-//    interface B1 extends C
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface B2 extends C
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface A extends B1, B2
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface B extends B1
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface F1
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface G1 extends F1
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface H1 extends G1
-//    {
-////        @OOPMultipleMethod
-////        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface F2
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface G2 extends F2
-//    {
-////        @OOPMultipleMethod
-////        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface H2 extends G2
-//    {
-////        @OOPMultipleMethod
-////        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface H3
-//    {
-//        @OOPMultipleMethod
-//        public void getfive();
-//    }
-//
-//    @OOPMultipleInterface
-//    interface I extends H1, H2, H3
-//    {
-//
-//    }
-//
-//    @OOPMultipleInterface
-//    interface I2 extends H1
-//    {
-//
-//    }
-
-
-//    public static void main(String[] args)
-//    {
-
-//        System.out.println(B.class.getMethods()[0].getDeclaringClass());
-//
-//        System.out.println((new MethodWrapper(F1.class.getMethods()[0])).equals((new MethodWrapper(G1.class.getMethods()[0]))));
-//
-//
-//        try
-//        {
-//            func(I.class);
-//        } catch (OOPCoincidentalAmbiguity e)
-//        {
-//            System.out.println("Failed OOPCoincidentalAmbiguity: " + e.getMessage());
-//        } catch (Exception e)
-//        {
-//            System.out.println("Failed Built In exception");
-//        }
-
-//        try
-//        {
-//            OOPMultipleControl.HasInherentAmbiguity(A.class);
-//        } catch (OOPInherentAmbiguity e)
-//        {
-//            System.out.println("Failed OOPInherentAmbiguity");
-////            System.out.println(e);
-//        } catch (OOPBadClass e)
-//        {
-//            System.out.println("Failed OOPBadClass");
-////            System.out.println(e);
-//        } catch (Excep
